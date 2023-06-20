a1fd6bb565e79a8b214d75f0adeeb87ba8189bf9
David Wu <david.wu@microlife.com.tw>
2023-06-20 10:00:49 +0800

    20230620 -add ocrad.js

--- OCRExample/Controllers/HomeController.cs
+++ OCRExample/Controllers/HomeController.cs
@@ -26,6 +26,10 @@ namespace OCRExample.Controllers
         {
             return View();
         }
+        public IActionResult OcradJs()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
